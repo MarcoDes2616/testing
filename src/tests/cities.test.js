35d25ba202c79c39d3d1444541b69ca523b6ddb4
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-05-08 11:00:42 -0500

    test

--- src/tests/cities.test.js
+++ src/tests/cities.test.js
@@ -2,47 +2,40 @@ const request = require("supertest")
 const app = require("../app");
 const Cities = require("../models/Cities");
 
-
-test("test get /cities should return status 200", async() => {
+let cityId;
+let data = {
+    "name": "Valencia",
+    "country": "Venezuela",
+    "isCapital": false
+}
+test("GET /cities should return status 200", async() => {
     const res = await request(app).get("/cities");
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveLength(2);
 })
 
-test("test post /cities should return status 201", async() => {
-    const data = {
-        "name": "Valencia",
-        "country": "Venezuela",
-        "isCapital": false
-    }
+test("POST /cities should return status 201", async() => {
     const res = await request(app).post("/cities").send(data);
+    cityId = res.body.id
     expect(res.statusCode).toBe(201);
     expect(res.body.name).toBe(data.name)
 })
 
-test("test put /cities/:id should return status 201", async() => {
-    const data = {
-        "name": "San Felipe",
-        "country": "Venezuela",
-        "isCapital": true
-    }
-    const res = await request(app).put("/cities/1").send(data);
+test("GET /cities/:id to getOne should return status 200", async() => {
+    const res = await request(app).get(`/cities/${cityId}`);
     expect(res.statusCode).toBe(200);
-    expect(res.body.isCapital).toBe(data.isCapital)
+    expect(res.body.name).toBe(data.name)
 })
 
-test("test getone /cities/:id should return status 200", async() => {
-    const res = await request(app).get("/cities/1");
+test("PUT /cities/:id should return status 201", async() => {
+    const res = await request(app).put(`/cities/${cityId}`).send({"isCapital": true});
     expect(res.statusCode).toBe(200);
-    expect(res.body).toMatchObject({
-        "name": "San Felipe",
-        "country": "Venezuela",
-        "isCapital": true
-    });
+    expect(res.body.isCapital).toBe(true)
 })
 
-test("test delete /cities/:id should return status 204", async() => {
-    const res = await request(app).delete("/cities/1");
+test("DELETE /cities/:id should return status 204", async() => {
+    const res = await request(app).delete(`/cities/${cityId}`);
     expect(res.statusCode).toBe(204);
-    // expect(res.body).toBe(1)
 })
+
+
